24e6e9163ddf96a441f4c35a11380361d7250ad8
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-08 14:19:42 -0600

    ejercicio 1

---
+++ actividad-01/app.js
@@ -0,0 +1,16 @@
+
+/*
+1. En consola del navegador, crea un pequeño programa que le pida al usuario que ingrese un número, multiplica
+ese número por 2 y muestra en salida el resultado de esa operación.
+*/
+
+/*
+const number = Number(prompt("Digite un número: "));
+
+let multiplication = 0;
+
+multiplication = number * 2;
+
+alert("El resultado es: " + multiplication);
+*/
+
