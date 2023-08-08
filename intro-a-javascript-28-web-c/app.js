ca0537a1fb211de7673ac1227dc19a872ca53b64
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-07 20:47:04 -0600

    Intro a variables y operadores en JS

--- intro-a-javascript-28-web-c/app.js
+++ intro-a-javascript-28-web-c/app.js
@@ -1,1 +1,7 @@
-console.log("Hola Mundo");
+const number1 = 2;
+const number2 = 10;
+
+const suma = number1 + number2;
+const multiplicacion = number1 * number2;
+console.log(suma);
+console.log(multiplicacion);
