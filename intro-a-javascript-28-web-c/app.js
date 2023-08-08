1c04786959beb197efce6b65e4174c319b53ea88
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-07 21:12:55 -0600

    practicando conversión y concatenación

--- intro-a-javascript-28-web-c/app.js
+++ intro-a-javascript-28-web-c/app.js
@@ -9,6 +9,8 @@ console.log(multiplicacion);
 
  //console.log('La suma de' + number1 + 'con' + number2 + ' = '+ suma);
 
+
+ /*
 const number1String = prompt('Dame un numero: ');
 const number2String = prompt('Dame otro numero: ');
 
@@ -23,3 +25,15 @@ console.log('la suma es: '+ sum + ', la mult: '+ multiplication + ', la resta es
 
 console.log(typeof number1);
 console.log(typeof number2);
+
+*/
+
+const number1String = prompt('Digite un numero: ');
+const number2String = prompt('Digite otro numero: ');
+
+const number1 = Number(number1String);
+const number2 = Number(number2String);
+
+const multiplicacion = number1 * number2;
+
+console.log('El resultado de multiplicar '+ number1 + ' x ' + number2 + ' es: ' + multiplicacion);
