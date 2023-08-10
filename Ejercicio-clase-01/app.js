3e458cca18ed2674d15125521040334fdf7ae8c9
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-09 19:22:42 -0600

    ejercio de clase 01

---
+++ Ejercicio-clase-01/app.js
@@ -0,0 +1,10 @@
+
+
+let number = Number(prompt("Ingrese un número: "));
+
+if (number % 2 === 0) {
+    alert("El numero " + number + " es PAR");
+} 
+else {
+    alert("El numero " + number + " es IMPAR");
+}
