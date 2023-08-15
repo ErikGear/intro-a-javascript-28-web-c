0657b1507aaeba3587aebad3a968b5fb77743061
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-14 18:49:46 -0600

    Ejercicio clase-03 Calculadora Básica

---
+++ Ejercicio-clase03/app.js
@@ -0,0 +1,24 @@
+
+var suma = 0;
+
+var num1 = parseFloat(prompt("Digite un numero: "));
+var num2 = parseFloat(prompt("Digite el segundo numero: "));
+
+
+var opcion = parseInt(prompt("Elija una opción: " + "\n 1. Suma " + "\n 2. Resta "));
+
+
+switch (opcion) {
+    case 1:
+        suma = num1 + num2;
+        prompt(`El resultado de sumar ${num1} + ${num2}  es: ${suma}`)
+        break;
+
+    case 2:
+        suma = num1 - num2;
+        prompt(`El resultado de restar ${num1} - ${num2}  es: ${suma}`);
+        break;
+    default:
+        alert(`La opción ${opcion} ingresada no es valida`);
+        break;
+}
