acd26ba8d88c7851f3f8d816492b839a51b47f6a
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-14 20:23:29 -0600

    Métodos REVERSE y CONCAT

--- Arreglos/app.js
+++ Arreglos/app.js
@@ -46,5 +46,25 @@ let porcionString = palabra.slice(0,4);
 console.log(porcionString);
 
 
+//Método splice, para agregar o bororar elementos en un array en pisicones determinadas
+ let frutas2 = ["pera", "manzana", "platano", "uvas", "mandarina"];
+ frutas2.splice(2, 0, "limon", "sandia");
 
+ // 2 --> indica el indice apartir del cual vamoas a insertar elementos nuevos en el arreglo
 
+ // 0 --> indica cuanmtoa elementos vamos a elliminar de nuestro arreglo
+
+ //Metodo REVERSE
+ let letters = ['a', 'b', 'c', 'd'];
+
+ letters.reverse();
+
+ console.log(letters);
+
+//Metodo CONCAT
+let numbers1 = [1, 2, 3, 4];
+let numbers2 = [5, 6, 7, 8];
+
+let numerosCompletos = numbers1.concat(numbers2);
+
+console.log(numerosCompletos);
