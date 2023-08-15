c3d5f5f3e0e3a09f3dd5b2bcd61f1c8521383767
ErikGear <erikgalindo23@aragon.unam.mx>
2023-08-14 19:36:37 -0600

    Metodo includes(search_element)

---
+++ Arreglos/app.js
@@ -0,0 +1,15 @@
+var verduras = ["Jitomate", "cebolla", "zanahoria"];
+
+console.log(verduras.length);
+
+//IndexOF --> retorna el indice del elemnto que estamos buscandoi
+// de no encontrarlo regresa un -1
+var indice = verduras.indexOf("Jitomate");
+
+console.log(indice);
+
+//includes("Elemento a buscar")
+var frutas = ["mango", "tuna", "uva"];
+
+let includesMango = frutas.includes("mango");
+console.log(includesMango);
